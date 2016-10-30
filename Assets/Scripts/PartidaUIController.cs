17fd4ffd93c67715abed5d1ed99a014c44becb9b
Bruna Faria <fariabruna71@gmail.com>
2016-10-30 19:39:59 -0200

    Primeiro commit - cenas criadas

---
+++ Assets/Scripts/PartidaUIController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PartidaUIController : MonoBehaviour{
+
+    public void Sair(){
+        SceneManager.LoadScene("inicio");
+    }
+}

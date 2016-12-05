d5d690b0a496db708d7128bcc242e6bc4adb4fac
Bruna Faria <fariabruna71@gmail.com>
2016-12-05 10:34:37 -0200

    Melhoria na usuabilidade

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -77,7 +77,7 @@ class GameController : MonoBehaviour {
         Util.instanciarPeao('h', 7, Cor.Preta, partida, peaoPreto);
     }
 
-    public void processarMouseDown(GameObject peca, GameObject casa){
+    public void processarMouseDown(GameObject obj, GameObject casa){
         if (estado == Estado.AguardandoJogada){
             if(casa != null){
                 try{
@@ -85,33 +85,37 @@ class GameController : MonoBehaviour {
                     int linha = casa.name[1] - '0';
                     origem = new PosicaoXadrez(coluna, linha);
                     partida.validarPosicaoDeOrigem(origem.toPosicao());
-                    pecaEscolhida = peca;
+                    pecaEscolhida = obj;
                     estado = Estado.Arrastando;
-                    txtMsg.text = "Solte a peça na casa de destino";
+                    txtMsg.text = "Selecione a casa de destino";
                 }
                 catch(TabuleiroException e){
                     informarAviso(e.Message);
                 }
             }
         }
-    }
+        else if (estado == Estado.Arrastando){
+            GameObject casaDestino = null;
+            if(obj.layer == LayerMask.NameToLayer("Casas")){
+                casaDestino = obj;
+            }
+            else{
+                casaDestino = casa;
+            }
 
-    public void processarMouseUp(GameObject peca, GameObject casa){
-        if (estado == Estado.Arrastando){
-            if (casa != null){
-                if (pecaEscolhida != null && pecaEscolhida == peca){
+            if (casaDestino != null && pecaEscolhida != null){
                     try{
-                        char coluna = casa.name[0];
-                        int linha = casa.name[1] - '0';
+                        char coluna = casaDestino.name[0];
+                        int linha = casaDestino.name[1] - '0';
                         destino = new PosicaoXadrez(coluna, linha);
 
                         partida.validarPosicaoDeDestino(origem.toPosicao(), destino.toPosicao());
                         Peca pecaCapturada = partida.realizaJogada(origem.toPosicao(), destino.toPosicao());
 
-                        if(pecaCapturada != null){
+                        if (pecaCapturada != null){
                             removerObjetoCapturado(pecaCapturada);
                         }
-                        peca.transform.position = Util.posicaoNaCena(coluna, linha);
+                        pecaEscolhida.transform.position = Util.posicaoNaCena(coluna, linha);
 
                         tratarJogadasEspeciais();
 
@@ -122,18 +126,17 @@ class GameController : MonoBehaviour {
                             txtMsg.text = "Vencedor: " + partida.jogadorAtual;
                             txtXeque.text = "XEQUEMATE";
                         }
-                        else {
+                        else{
                             estado = Estado.AguardandoJogada;
                             informarAguardando();
                             txtXeque.text = (partida.xeque) ? "XEQUE" : "";
                         }
                     }
                     catch (TabuleiroException e){
-                        peca.transform.position = Util.posicaoNaCena(origem.coluna, origem.linha);
+                        pecaEscolhida.transform.position = Util.posicaoNaCena(origem.coluna, origem.linha);
                         estado = Estado.AguardandoJogada;
                         informarAviso(e.Message);
                     }
-                }
             }
         }
     }
--- Assets/Scripts/Movimentacao.cs
+++ Assets/Scripts/Movimentacao.cs
@@ -7,14 +7,10 @@ public class Movimentacao : MonoBehaviour {
     bool colidindo;
     GameObject casa;
 
-    int chaoMask;
-
-    void Awake(){
+   void Awake(){
         gameController = FindObjectOfType(typeof(GameController)) as GameController;
         colidindo = false;
         casa = null;
-        
-        chaoMask = LayerMask.GetMask("Chao");
     }
 
     void OnTriggerEnter(Collider other){
@@ -33,18 +29,5 @@ public class Movimentacao : MonoBehaviour {
 
     void OnMouseDown(){
         gameController.processarMouseDown(transform.gameObject, casa);
-    }
-    void OnMouseUp(){
-        gameController.processarMouseUp(transform.gameObject, casa);
-    }
-    
-    void OnMouseDrag(){
-        if(gameController.estado == Estado.Arrastando && gameController. pecaEscolhida == transform.gameObject){
-            Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(raio, out hit, 300f, chaoMask)){
-                transform.position = hit.point;
-            }
-        }
     }
 }
